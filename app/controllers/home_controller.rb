828facaab58571638e87174abf710d8d79a019d4
Thao <theo@futureworkz.com>
2016-05-07 19:44:45 +0700

    order by created_at DESC

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @shoe_filter = ShoeFilter.new(shoe_filter_params)
-    @shoes = @shoe_filter.result.paginate(page: params[:page], per_page: 5)
+    @shoes = @shoe_filter.result.order('created_at DESC').paginate(page: params[:page], per_page: 5)
   end
 
   protected
