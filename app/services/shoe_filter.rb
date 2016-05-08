ca7492ff4c4fcad95c1c5b72cde5a2f7f9719e4e
Thao <theo@futureworkz.com>
2016-05-08 22:09:42 +0700

    update shoe_filter_spec#result

--- app/services/shoe_filter.rb
+++ app/services/shoe_filter.rb
@@ -6,16 +6,16 @@ class ShoeFilter
 
   def initialize(option)
     return if option.nil?
-    @brand_ids     = option[:brand_ids]
-    @price_from   = option[:price_from].to_f
-    @price_to     = option[:price_to].to_f
+    @brand_ids      = option[:brand_ids]
+    @price_from     = option[:price_from].to_f
+    @price_to       = option[:price_to].to_f
   end
 
   def result
     shoes = Shoe.all
-    shoes = shoes.find_by_brand_ids(brand_ids) if brand_ids.present?
-    shoes = shoes.find_by_price_from(price_from) if price_from.present?
-    shoes = shoes.find_by_price_to(price_to) if price_to.present?
+    shoes = shoes.find_by_brand_ids(brand_ids)    if brand_ids.present?
+    shoes = shoes.find_by_price_from(price_from)  if price_from.present?
+    shoes = shoes.find_by_price_to(price_to)      if price_to.present?
     shoes
   end
 
--- spec/services/shoe_filter_spec.rb
+++ spec/services/shoe_filter_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe ShoeFilter do
   describe '#result' do
     let!(:brand)             { create(:brand, title: 'Nike') }
-    let!(:shoe)             { create(:shoe, name: 'X-Men', brand: brand) }
+    let!(:shoe)             { create(:shoe, name: 'X-Men', price: 10, brand: brand) }
 
     it 'returns a list of shoes found' do
-      shoes = ShoeFilter.new(brand_ids: [brand.id]).result
+      shoes = ShoeFilter.new(brand_ids: brand.id, price_to: 10).result
+      
       expect(shoes).to include shoe
     end
   end
